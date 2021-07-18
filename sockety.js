a93670edb01e60756995b9f661a2a859508c9fe5
KinoKiru <Youriboulanger@gmail.com>
2021-07-18 21:38:27 +0200

    errorAdded

--- sockety.js
+++ sockety.js
@@ -1,17 +1,18 @@
 const socket = io("http://143.178.215.190:3000");
 let table = document.getElementById("commandsTable");
-
+let text = document.getElementById("bottext");
 
 socket.on("initStartup" , (rows)=>{
- fillTable(rows)
+ fillTable(rows, text)
 })
 
 socket.on("update", (rows)=>{
      table.innerHTML = "";
-     fillTable(rows)
+     fillTable(rows, text)
 })
 
 function fillTable(rows){
+   text.classList.add("d-none");
    for (const row of rows) {
       let tr = document.createElement("tr");
       let td = document.createElement("td");
