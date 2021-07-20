82f88d502d3c2fc849c5694b49d353ee22b17e55
KinoKiru <Youriboulanger@gmail.com>
2021-07-20 19:56:06 +0200

    oNoCringeee

--- socket.io.js
+++ socket.io.js
@@ -1,15 +1,16 @@
 let locationUrl = "://143.178.215.190:3001/socket.io/socket.io.js"
-jQuery.loadScript = function (url, callback) {
-    jQuery.ajax({
-        url: url,
-        dataType: 'script',
-        success: callback,
-        async: true
-    });
-}
+const script = document.createElement('script');
 
-if(location.href.startsWith("https")){
-    $.loadScript("https" + locationUrl)
-}else{
-    $.loadScript("http" + locationUrl.replace("3001", "3000"))
+if (location.href.startsWith("https")) {
+    script.src = "https" + locationUrl;
+} else {
+    script.src = "http" + locationUrl.replace("3001", "3000");
 }
+// Append to the `head` element
+document.head.appendChild(script);
+
+script.addEventListener('load', ()=> {
+    const script = document.createElement('script');
+    script.src = "sockety.js";
+    document.head.appendChild(script);
+});
