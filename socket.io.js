bdbdbd0937eb42098a1410111e2c0d4d1217801b
KinoKiru <Youriboulanger@gmail.com>
2021-07-20 19:43:40 +0200

    noSoyIncente

--- socket.io.js
+++ socket.io.js
@@ -2,7 +2,7 @@ let locationUrl = "://143.178.215.190:3001/socket.io/socket.io.js"
 
 
 if(location.href.startsWith("https")){
-    document.head.innerHTML += "<script src='"+ "https" + locationUrl+"' defer>";
+    document.head.innerHTML += "<script src='"+ "https" + locationUrl+"'>";
 }else{
-    document.head.innerHTML += "<script src='"+ "http" + locationUrl.replace("3001", "3000")+"' defer>";
+    document.head.innerHTML += "<script src='"+ "http" + locationUrl.replace("3001", "3000")+"'>";
 }
