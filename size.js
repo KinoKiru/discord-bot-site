18b1feaad4fb5f4d68c31890849e80ad626028e4
KinoKiru <Youriboulanger@gmail.com>
2021-07-22 23:28:27 +0200

    jaman

--- size.js
+++ size.js
@@ -1,12 +1,20 @@
-if (window.innerWidth < 506){
 
+
+window.addEventListener('resize', function(event) {
     let thUsages = document.querySelector("table>thead.thead-dark>tr>th.usages")
     let tdUsages = document.querySelectorAll("table>tbody>tr>td.usages");
+    if (window.innerWidth < 506){
 
-    thUsages.classList.add("d-none");
-    for (const tdUsage of tdUsages) {
-        tdUsage.classList.add("d-none");
+        thUsages.classList.add("d-none");
+        for (const tdUsage of tdUsages) {
+            tdUsage.classList.add("d-none");
+        }
+    }else{
+        thUsages.classList.remove("d-none");
+        for (const tdUsage of tdUsages) {
+            tdUsage.classList.remove("d-none");
+        }
     }
-}else{
+}, true);
 
-}
+
