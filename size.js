c552d476d92c5d02bb8dcd76460081d86b24cbe8
KinoKiru <Youriboulanger@gmail.com>
2021-07-22 23:15:15 +0200

    jaman

--- size.js
+++ size.js
@@ -2,6 +2,9 @@ if (window.innerWidth < 506){
     let thUsages = document.querySelector("table>thead.thead-dark>th.usages")
     let tdUsages = document.querySelectorAll("table>tbody>td.usages");
 
+    console.log(thUsages);
+    console.log(tdUsages);
+
     thUsages.classList.add("d-none");
     for (const tdUsage of tdUsages) {
         tdUsage.classList.add("d-none");
